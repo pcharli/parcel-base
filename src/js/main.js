e8ae5f367c531a4427a02ee62863c0bcd49cb5e5
Pierre Charlier <chrprr1@forem.be>
2020-06-10 16:24:08 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-import 'bulma'
-//import Alert from 'alert'
-import '../scss/style.scss'
-
-// import('../js/header').then(function(header) {
-//     header.render()
-// })
-
-//Alert("essai")
-
-
-
-
 const text = "Hello World"
 
 alert(text)
